33f27897f2bd52d1e891b77ce1bc5d9822b45e87
lakshan <49824628+Rasi98@users.noreply.github.com>
2021-06-10 19:08:18 +0530

    First branch commit

--- wfh_companion/lib/main.dart
+++ wfh_companion/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
+        // TEST COMMENT
         //
         // Try running your application with "flutter run". You'll see the
         // application has a blue toolbar. Then, without quitting the app, try
