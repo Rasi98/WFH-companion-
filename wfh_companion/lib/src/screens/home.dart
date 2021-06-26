e75be0f2fc55482e4f0baeb7102d8266f3762e02
Lakshan Rasingolla <rwmlakshan@gmail.com>
2021-06-27 00:42:22 +0530

    signin
    Signin Authentication

--- wfh_companion/lib/src/screens/home.dart
+++ wfh_companion/lib/src/screens/home.dart
@@ -1,8 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:wfh_companion/src/screens/login.dart';
 
 class HomeScreen extends StatelessWidget {
+  final auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Home'),
+        ),
+        body: Center(
+          child: FlatButton(
+              onPressed: () {
+                auth.signOut();
+                Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context) => LoginScreen()));
+              },
+              child: Text('Signout')),
+        ));
   }
 }
