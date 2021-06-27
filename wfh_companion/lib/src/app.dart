d80a321dfb08f9d3495219719041fddabe7e98f4
thilina15 <thibbatz@gmail.com>
2021-06-27 10:49:25 +0530

    skip login wehn user already login

--- wfh_companion/lib/src/app.dart
+++ wfh_companion/lib/src/app.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:wfh_companion/src/screens/home.dart';
 import 'package:wfh_companion/src/screens/login.dart';
 
 class App extends StatelessWidget {
@@ -8,7 +10,9 @@ class App extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'WHF-Companion',
-      home: LoginScreen(),
+      home: FirebaseAuth.instance.currentUser == null
+          ? LoginScreen()
+          : HomeScreen(),
     );
   }
 }
