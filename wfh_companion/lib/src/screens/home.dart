72ae56db4ef5439d54c6cc34ed24329a10b4e9b0
thilina15 <thibbatz@gmail.com>
2021-07-12 13:37:23 +0530

    responsive error fixed

--- wfh_companion/lib/src/screens/home.dart
+++ wfh_companion/lib/src/screens/home.dart
@@ -13,109 +13,115 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [
-        SizedBox(height: MediaQuery.of(context).size.height * .04),
-        Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height * .12,
-          decoration: BoxDecoration(
-            color: Color.fromRGBO(159, 148, 6, 1),
+        body: SingleChildScrollView(
+      child: Column(
+        children: [
+          SizedBox(height: MediaQuery.of(context).size.height * .04),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * .12,
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(159, 148, 6, 1),
+            ),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * .01,
+                ),
+                Row(
+                  children: [
+                    SizedBox(width: MediaQuery.of(context).size.width * .1),
+                    Container(
+                      //home image
+                      width: MediaQuery.of(context).size.width * .16,
+                      height: MediaQuery.of(context).size.height * .1,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              fit: BoxFit.fitWidth,
+                              image: AssetImage('assets/homeLogo.png'))),
+                    ),
+                    SizedBox(width: MediaQuery.of(context).size.width * .15),
+                    Text(
+                      'John Moors',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    IconButton(
+                      icon: Image.asset('assets/d.png'),
+                      iconSize: 55,
+                      onPressed: () {
+                        auth.signOut();
+                        Navigator.of(context).pushReplacement(MaterialPageRoute(
+                            builder: (context) => LoginScreen()));
+                      },
+                    )
+                  ],
+                )
+              ],
+            ),
+          ), //end of cunsom app bar
+          SizedBox(
+            height: MediaQuery.of(context).size.height * .09,
           ),
-          child: Column(
+          Row(
             children: [
               SizedBox(
-                height: MediaQuery.of(context).size.height * .01,
+                width: MediaQuery.of(context).size.width * .15,
               ),
-              Row(
-                children: [
-                  SizedBox(width: MediaQuery.of(context).size.width * .1),
-                  Container(
-                    //home image
-                    width: MediaQuery.of(context).size.width * .16,
-                    height: MediaQuery.of(context).size.height * .1,
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                            fit: BoxFit.fitWidth,
-                            image: AssetImage('assets/homeLogo.png'))),
-                  ),
-                  SizedBox(width: MediaQuery.of(context).size.width * .15),
-                  Text(
-                    'John Moors',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  IconButton(
-                    icon: Image.asset('assets/d.png'),
-                    iconSize: 55,
-                    onPressed: () {
-                      auth.signOut();
-                      Navigator.of(context).pushReplacement(MaterialPageRoute(
-                          builder: (context) => LoginScreen()));
-                    },
-                  )
-                ],
-              )
+              MainMenuCard(
+                asset: "assets/notification.png",
+                text: "Notification",
+                onClick: () {},
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .1,
+              ),
+              MainMenuCard(
+                  asset: "assets/fitness.png", text: "Fitness", onClick: () {})
             ],
           ),
-        ), //end of cunsom app bar
-        SizedBox(
-          height: MediaQuery.of(context).size.height * .09,
-        ),
-        Row(
-          children: [
-            SizedBox(
-              width: MediaQuery.of(context).size.width * .15,
-            ),
-            MainMenuCard(
-              asset: "assets/notification.png",
-              text: "Notification",
-              onClick: () {},
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * .1,
-            ),
-            MainMenuCard(
-                asset: "assets/fitness.png", text: "Fitness", onClick: () {})
-          ],
-        ),
-        SizedBox(
-          height: MediaQuery.of(context).size.height * .02,
-        ),
-        Row(
-          children: [
-            SizedBox(
-              width: MediaQuery.of(context).size.width * .15,
-            ),
-            MainMenuCard(
-                asset: "assets/time.png", text: "Check In/Out", onClick: () {}),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * .1,
-            ),
-            MainMenuCard(
-                asset: "assets/track.png", text: "Tracking", onClick: () {})
-          ],
-        ),
-        SizedBox(
-          height: MediaQuery.of(context).size.height * .02,
-        ),
-        Row(
-          children: [
-            SizedBox(
-              width: MediaQuery.of(context).size.width * .15,
-            ),
-            MainMenuCard(
-                asset: "assets/message.png", text: "Message", onClick: () {}),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * .1,
-            ),
-            MainMenuCard(
-                asset: "assets/settings.png", text: "Settings", onClick: () {})
-          ],
-        ),
-      ],
+          SizedBox(
+            height: MediaQuery.of(context).size.height * .02,
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .15,
+              ),
+              MainMenuCard(
+                  asset: "assets/time.png",
+                  text: "Check In/Out",
+                  onClick: () {}),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .1,
+              ),
+              MainMenuCard(
+                  asset: "assets/track.png", text: "Tracking", onClick: () {})
+            ],
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * .02,
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .15,
+              ),
+              MainMenuCard(
+                  asset: "assets/message.png", text: "Message", onClick: () {}),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .1,
+              ),
+              MainMenuCard(
+                  asset: "assets/settings.png",
+                  text: "Settings",
+                  onClick: () {})
+            ],
+          ),
+        ],
+      ),
     ));
   }
 }
